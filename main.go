c5f8e7d6bdf5a266b57f3b66d2253a1901496108
Michael Vogt <mvo@debian.org>
2013-11-10 17:56:12 +0100

    cleanup main.go and add --with-auth and --with-multithreading

--- main.go
+++ main.go
@@ -48,6 +48,23 @@ func openSmbfile(client *libsmbclient.Client, furi string) {
 	f.Close()
 }
 
+func askAuth(server_name, share_name string)(out_domain, out_username, out_password string) { 
+	bio := bufio.NewReader(os.Stdin)
+	fmt.Printf("auth for %s %s\n", server_name, share_name)
+	// domain
+	fmt.Print("Domain: ")
+	domain, _, _ := bio.ReadLine()
+	// read username
+	fmt.Print("Username: ")
+	username, _, _ := bio.ReadLine()
+	// read pw from stdin
+	fmt.Print("Password: ")
+	setEcho(false)
+	password, _, _ := bio.ReadLine()
+	setEcho(true)
+	return strings.TrimSpace(string(domain)), strings.TrimSpace(string(username)), strings.TrimSpace(string(password))
+}
+
 func setEcho(terminal_echo_enabled bool) {
 	var cmd *exec.Cmd
 	if terminal_echo_enabled {
@@ -60,30 +77,33 @@ func setEcho(terminal_echo_enabled bool) {
 
 func main() {
 	var duri, furi string
+	var withAuth, withMultithread bool
 	flag.StringVar(&duri, "show-dir", "", "smb://path/to/dir style directory")
 	flag.StringVar(&furi, "show-file", "", "smb://path/to/file style file")
+	flag.BoolVar(&withAuth, "with-auth", false, "ask for auth")
+	flag.BoolVar(&withMultithread, "with-multithread", false, "mulithread")
 	flag.Parse()
 
 	client := libsmbclient.New()
 	//client.SetDebug(99)
 
-	fn := func(server_name, share_name string)(domain, username, password string) {
-		fmt.Printf("auth for %s %s: ", server_name, share_name)
-		// read pw from stdin
-		setEcho(false)
-		bio := bufio.NewReader(os.Stdin)
-		pw, _, _ := bio.ReadLine()
-		setEcho(true)
-		return "URT", "vogtm", strings.TrimSpace(string(pw))
+	if withAuth {
+		client.SetAuthCallback(askAuth)
 	}
-	client.SetAuthCallback(fn)
 
+	var fn func(*libsmbclient.Client, string)
+	var uri string
 	if duri != "" {
-		openSmbdir(client, duri)
+		fn = openSmbdir
+		uri = duri
 	} else if furi != "" {
-		openSmbfile(client, furi)
+		fn = openSmbfile
+		uri = furi
 	}
-
-
+	if withMultithread {
+		go fn(client, uri)
+		go fn(client, uri)
+	}
+	fn(client, uri)
 
 }
