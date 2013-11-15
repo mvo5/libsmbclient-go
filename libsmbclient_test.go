6057ce606b7616252cff6c9f545152e6edb4c06f
Michael Vogt <mvo@debian.org>
2013-11-15 23:31:31 +0100

    make (very basic) test work

--- libsmbclient_test.go
+++ libsmbclient_test.go
@@ -8,53 +8,71 @@ import (
 	"os/exec"
 	"path/filepath"
 	"io"
+	_ "time"
+	"text/template"
 )
 
-type SmbdContext struct {
-	smbd *exec.Cmd
-}
+var teardown []func()
 
 func generateSmbdConf() string {
-	// FIXME: temp race
-	tmpdir := "/tmp/samba"
-	os.RemoveAll(tmpdir)
-	os.Mkdir(tmpdir, 0755)
-	os.Mkdir(filepath.Join(tmpdir, "private"), 0755)
-	os.Mkdir(filepath.Join(tmpdir, "public"), 0755)
-	f, err := os.Create(filepath.Join(tmpdir, "smbd.conf"))
+	tempdir, _ := filepath.Abs("./tmp/samba")
+	teardown = append(teardown, func() {
+		os.RemoveAll(tempdir)
+	})
+
+	paths := [...]string{
+		tempdir, 
+		filepath.Join(tempdir, "samaba", "private"),
+		filepath.Join(tempdir, "samba", "public"),
+	}
+	for _, d := range(paths) {
+		err := os.MkdirAll(d, 0755)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	os.Mkdir(filepath.Join(tempdir, "private"), 0755)
+	os.Mkdir(filepath.Join(tempdir, "public"), 0755)
+	f, err := os.Create(filepath.Join(tempdir, "smbd.conf"))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
-	// FIXME: generate on the fly via template
-	s := `[global]
+	templateText := `[global]
 workgroup = TESTGROUP
 interfaces = lo 127.0.0.0/8
 smb ports = 1445
 log level = 2
 map to guest = Bad User
 passdb backend = smbpasswd
-smb passwd file = /tmp/samba/smbpasswd
-lock directory = /tmp/samba/intern
-state directory = /tmp/samba/intern
-cache directory = /tmp/samba/intern
-pid directory = /tmp/samba/intern
-private dir = /tmp/samba/intern
-ncalrpc dir = /tmp/samba/intern
+smb passwd file = {{.Tempdir}}/smbpasswd
+lock directory = {{.Tempdir}}/intern
+state directory = {{.Tempdir}}/intern
+cache directory = {{.Tempdir}}/intern
+pid directory = {{.Tempdir}}/intern
+private dir = {{.Tempdir}}/intern
+ncalrpc dir = {{.Tempdir}}/intern
 
 [public]
-path = /tmp/samba/public
+path = {{.Tempdir}}/public
 guest ok = yes
 
 [private]
-path = /tmp/samba/private
+path = {{.Tempdir}}/private
 read only = no
-`
-	f.Write([]byte(s))
+`	
+	type Dir struct {
+		Tempdir string
+	}
+	t, err := template.New("smb-conf").Parse(templateText)
+	if err != nil {
+		log.Fatal(err)
+	}
+	t.Execute(f, Dir{tempdir})
 	return f.Name()
 }
 
-func startSmbd() (SmbdContext, io.Reader) {
+func startSmbd() io.Reader {
 	// thanks pitti :)
 	os.Setenv("LIBSMB_PROG", "nc localhost 1445")
 	smb_conf := generateSmbdConf()
@@ -63,36 +81,67 @@ func startSmbd() (SmbdContext, io.Reader) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	smbdContext := SmbdContext{smbd: cmd}
 	cmd.Start()
-	return smbdContext, stdout
+	teardown = append(teardown, func() {
+		cmd.Process.Kill()
+		cmd.Process.Wait()
+	})
+	return stdout
 }
 
-func (s *SmbdContext) Stop() {
-	s.smbd.Process.Kill()
-	s.smbd.Process.Wait()
+func setUp() {
+	stdout := startSmbd()
+	//time.Sleep(100 * time.Millisecond)
+	_ = stdout
+	//fmt.Println(stdout)
+	//go io.Copy(os.Stdout, stdout)
+}
+
+func tearDown() {
+	// cleanup
+	for _,f := range teardown {
+		f()
+	}
+	// cleanup atexit
+	teardown = []func(){}
+}
+
+func sliceContains(slice []string, needle string) bool {
+	for _, d := range(slice) {
+		if d == needle {
+			return true
+		}
+	}
+	return false
 }
 
 func TestLibsmbclientBindings(t *testing.T) {
-	fmt.Println("Start libsmbclient test")
-	smbdContext, stdout := startSmbd()
+	fmt.Println("libsmbclient opendir test")
 
-	// keep us updated :)
-	//go io.Copy(os.Stdout, stdout)
+	setUp()
 
+	// open client
 	client := New()
 	d, err := client.Opendir("smb://localhost")
 	if err != nil {
 		t.Error("failed to open localhost ", err)
 	}
+	
+	// collect dirs
+	foundSmbDirs := make([]string, 10)
 	for {
 		dirent, err := d.Readdir()
 		if err != nil {
 			break
 		}
-		fmt.Println(dirent)
+		foundSmbDirs = append(foundSmbDirs, dirent.Name)
+	}
+	// check for expected data
+	for _, needle := range([]string{"private", "public"}) {
+		if !sliceContains(foundSmbDirs, needle) {
+			t.Error("missing 'public' folder (%v)", foundSmbDirs)
+		}
 	}
 
-	fmt.Print(stdout)
-	smbdContext.Stop()
+	tearDown()
 }
