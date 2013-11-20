78c399689e12d8338f3f02f02ba42471ca5fb5c2
Michael Vogt <mvo@debian.org>
2013-11-20 17:15:05 +0100

    add threads test-case, THREADS=2 is ok, bigger numbers crash - so we need to figure out why :)

--- libsmbclient_test.go
+++ libsmbclient_test.go
@@ -8,37 +8,13 @@ import (
 	"os/exec"
 	"path/filepath"
 	"io"
+	"io/ioutil"
 	_ "time"
 	"text/template"
+	"math/rand"
 )
 
-var teardown []func()
-
-func generateSmbdConf() string {
-	tempdir, _ := filepath.Abs("./tmp/samba")
-	teardown = append(teardown, func() {
-		os.RemoveAll(tempdir)
-	})
-
-	paths := [...]string{
-		tempdir, 
-		filepath.Join(tempdir, "samaba", "private"),
-		filepath.Join(tempdir, "samba", "public"),
-	}
-	for _, d := range(paths) {
-		err := os.MkdirAll(d, 0755)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-	os.Mkdir(filepath.Join(tempdir, "private"), 0755)
-	os.Mkdir(filepath.Join(tempdir, "public"), 0755)
-	f, err := os.Create(filepath.Join(tempdir, "smbd.conf"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-	templateText := `[global]
+var SMB_CONF_TEMPLATE = `[global]
 workgroup = TESTGROUP
 interfaces = lo 127.0.0.0/8
 smb ports = 1445
@@ -61,6 +37,34 @@ guest ok = yes
 path = {{.Tempdir}}/private
 read only = no
 `	
+
+// global teardown funcitons
+var teardown []func()
+
+func generateSmbdConf() string {
+	tempdir, _ := filepath.Abs("./tmp/samba")
+	teardown = append(teardown, func() {
+		os.RemoveAll(tempdir)
+	})
+	paths := [...]string{
+		tempdir, 
+		filepath.Join(tempdir, "samaba", "private"),
+		filepath.Join(tempdir, "samba", "public"),
+	}
+	for _, d := range(paths) {
+		err := os.MkdirAll(d, 0755)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	os.Mkdir(filepath.Join(tempdir, "private"), 0755)
+	os.Mkdir(filepath.Join(tempdir, "public"), 0755)
+	f, err := os.Create(filepath.Join(tempdir, "smbd.conf"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	templateText := SMB_CONF_TEMPLATE
 	type Dir struct {
 		Tempdir string
 	}
@@ -76,7 +80,7 @@ func startSmbd() io.Reader {
 	// thanks pitti :)
 	os.Setenv("LIBSMB_PROG", "nc localhost 1445")
 	smb_conf := generateSmbdConf()
-	cmd := exec.Command("smbd", "-iFS", "-s", smb_conf)
+	cmd := exec.Command("smbd", "-FS", "-s", smb_conf)
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
 		log.Fatal(err)
@@ -130,6 +134,77 @@ func TestLibsmbclientBindings(t *testing.T) {
 	// check for expected data
 	if !foundSmbDirs["private"] || !foundSmbDirs["public"] {
 		t.Error(fmt.Sprintf("missing excpected folder in (%v)", foundSmbDirs))
+	}
+
+	tearDown()
+}
+
+
+func getRandomFileName() string {
+	return fmt.Sprintf("%x", rand.Int())
+}
+
+func openFile(client *Client, path string, c chan int) {
+	f, err := client.Open(path, 0, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		f.Close()
+		c <- 1
+	}()
+
+	// FIXME: move this into the lib as ioutil.ReadFile()
+	buf := make([]byte, 64*1024)
+	for {
+		_, err := f.Read(buf)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Print(err)
+			return
+		}
+	}
+}
+
+func TestLibsmbclientThreaded(t *testing.T) {
+	fmt.Println("libsmbclient threaded test")
+
+	setUp()
+
+	THREADS := 2
+	FILE_SIZE := 16*1024
+
+	// create a bunch of test files
+	buf := make([]byte, FILE_SIZE)
+	for i := 0; i < THREADS; i++ {
+		tmpf := "./tmp/samba/public/" + getRandomFileName()
+		ioutil.WriteFile(tmpf, buf, 0644)
+	}
+
+	// open client
+	baseDir := "smb://localhost/public/"
+	client := New()
+	d, err := client.Opendir(baseDir)
+	if err != nil {
+		t.Error("failed to open localhost ", err)
+	}
+	// read all files threaded
+	c := make(chan int)
+	count := 0
+	for {
+		dirent, err := d.Readdir()
+		if err != nil {
+			break
+		}
+		if dirent.Type == SMBC_FILE {
+			go openFile(client, baseDir+dirent.Name, c)
+		}
+	}
+	
+	for count < THREADS {
+		count += <- c
 	}
 
 	tearDown()
